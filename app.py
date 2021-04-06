208b06e360d24c217077a07c91019f358c40fdae
Michael Hayman <mdghayman@gmail.com>
2021-04-06 01:02:19 +0100

    - Updates to code and app, with addition of tests.

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import streamlit as st
-import pandas as pd
 import os
 import json
-from functions import cache_countries, price_counter, currency_converter
+from functions import cache_countries, currency_converter
 
 
 if not os.path.isfile('countries_details.txt'):
     cache_countries()
+
+input_file = 'pricing.json'
+customer_country = 'United Kingdom'
 
 countries_list = []
 with open('countries_details.txt', 'r') as f:
@@ -14,25 +16,73 @@ with open('countries_details.txt', 'r') as f:
     for key, value in countries_details['results'].items():
         countries_list.append(value['name'])
 
-input_file = 'pricing.json'
-customer_country = 'local'
 
-st.markdown("<h2 style='text-align: center; color: grey'>\
-    Tray.io challenge</h2>", unsafe_allow_html=True)
-st.markdown("<p style='text-align: center'>\
-    Algorithmically predict the SaaS product that will generate more interest \
-    and traffic, to proactively create integrations for unicorns.</p>", \
+col1, col2, col3 = st.beta_columns(3)
+
+col1.markdown("<h3 style='text-align: right; color: purple;'>\
+    Michael Hayman</h3>",
     unsafe_allow_html=True)
 
-if st.button('Select country for foreign customer'):
-    customer_country = st.selectbox(label='Please enter customer country \
-        (leave this blank if customer is local)', options=countries_list)
+col1.markdown("<h3 style='text-align: right; color: purple;'>\
+    Data Engineer</h3>",
+    unsafe_allow_html=True)
+
+col1.markdown("<h3 style='text-align: right; color: purple;'>\
+    Coding Test</h3>",
+    unsafe_allow_html=True)
+
+col2.image('images/TailsLogo.png', use_column_width=True, \
+    output_format='PNG')
+
+col3.image('images/MichaelHayman.jpeg', width=165, \
+    output_format='JPEG')
+
+
+st.markdown("<h2 style='text-align: center; color: grey'>\
+    Summary</h2>", unsafe_allow_html=True)
+
+st.markdown("<p style='text-align: center'>\
+    The brief required me to recreate a hypothetical version of the internal \
+    pricing service at Tails. This is realised here with a simple user \
+    interface. The relevant functions are also stored in the python script \
+    underlying this app, and so can be intergrated into a wider data pipeline. \
+    </p>", unsafe_allow_html=True)
+
+
+st.markdown("<h3 style='text-align: left; color: grey'>\
+    Step 1 (optional): Upload new input file</h3>", unsafe_allow_html=True)
+
+st.markdown("<p style='text-align: left'>\
+    The file saved as `pricing.json` in this app directory will be used by \
+    default. Any new file uploaded below will be saved as `pricing.json` in \
+    this app directory.\
+    </p>", unsafe_allow_html=True)
 
 if st.button('Upload new input file'):
     input_file = st.file_uploader('Please drag and drop a text file with your \
         JSON inputs.')
 
+
+st.markdown("<h3 style='text-align: left; color: grey'>\
+    Step 2: Select customer country</h3>", unsafe_allow_html=True)
+
+customer_country = st.selectbox(label='Customer country (leave this blank if \
+    customer is from United Kingdom).', options=['<SELECT>']+countries_list)
+
+if customer_country == '<SELECT>':
+    customer_country = 'United Kingdom'
+
+st.markdown(f"<p style='text-align: left'>\
+    <b>{customer_country}</b> currently selected. \
+    </p>", unsafe_allow_html=True)
+
+
+st.markdown("<h3 style='text-align: left; color: grey'>\
+    Step 3: Calculate output</h3>", unsafe_allow_html=True)
+
+st.markdown("<p style='text-align: left'>\
+    Click here to see the JSON output. \
+    </p>", unsafe_allow_html=True)
+
 if st.button('Calculate output'):
-    if customer_country != 'local':
-        st.write(currency_converter(customer_country))
-    st.write(price_counter(input_file))
+    st.write(currency_converter(input_file, customer_country))
--- functions.py
+++ functions.py
@@ -1,38 +1,71 @@
 import json
 import requests
 
+
+api_key = 'c6d454bc0e16fa560639'
+
+
 def cache_countries():
+
     countries_url = 'https://free.currconv.com/api/v7/countries?' + \
-        'apiKey=c6d454bc0e16fa560639'
+        f'apiKey={api_key}'
+
     countries_details = requests.get(countries_url).json()
+
     with open('countries_details.txt', 'w') as outfile:
         json.dump(countries_details, outfile)
 
-def currency_converter(customer_country):
+    return countries_details
+
+
+def currency_converter(input_file, customer_country):
+
+    customer_currency = 'GBP'
+
     with open('countries_details.txt', 'r') as f:
         countries_details = json.load(f)
+
     for key, value in countries_details['results'].items():
         if customer_country == value['name']:
             customer_currency = value['currencyId']
+
+    exchange_rate_code = f'GBP_{customer_currency}'
+
     exchange_rate_url = 'https://free.currconv.com/api/v7/convert?q=' + \
         f'GBP_{customer_currency}' + \
-        '&compact=ultra&apiKey=c6d454bc0e16fa560639'
-    exchange_rate_details = requests.get(exchange_rate_url).json()
-    return exchange_rate_details
+        '&compact=ultra&' + \
+        f'apiKey={api_key}'
 
-def price_counter(input_file):
-    total_price = 0
-    total_vat = 0
+    exchange_rate_details = requests.get(exchange_rate_url).json()
+    exchange_rate = exchange_rate_details[exchange_rate_code]
+
+    total_price_GBP = 0
+    total_vat_GBP = 0
+    total_price_customer_currency = 0
+    total_vat_customer_currency = 0
     input_text = open(input_file).read()
     input_json = json.loads(input_text)
     details_per_product = {}
+
     for product in input_json['prices']:
+        product['price_GBP'] = product['price']
+        product[f'price_{customer_currency}'] = round(product['price'] * exchange_rate)
+        del product['price']
         for key, value in input_json['vat_bands'].items():
             if product['vat_band'] == key:
                 product['vat'] = value
         del product['vat_band']
-        total_price += product['price']
-        total_vat += product['price'] * product['vat']
-    return {'total_price': total_price,
-        'total_vat': total_vat,
-        'products': input_json['prices']}
+        total_price_GBP += product['price_GBP']
+        total_vat_GBP += product['price_GBP'] * product['vat']
+        total_price_customer_currency += product[f'price_{customer_currency}']
+        total_vat_customer_currency += product[f'price_{customer_currency}'] * product['vat']
+
+    return {'customer_country': customer_country,
+        'customer_currency': customer_currency,
+        'exchange_rate': exchange_rate,
+        'total_price_GBP': total_price_GBP,
+        'total_vat_GBP': round(total_vat_GBP),
+        f'total_price_{customer_currency}': total_price_customer_currency,
+        f'total_vat_{customer_currency}': round(total_vat_customer_currency),
+        'products': input_json['prices'],
+    }
